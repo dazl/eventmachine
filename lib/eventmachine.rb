4679c45056e9c0be704e8aec4a987be491496d52
dazl <davide@medeo.ca>
2013-07-25 14:41:11 -0700

    add ext folder -- workaround for Unable to load the EventMachine C extension error

--- lib/eventmachine.rb
+++ lib/eventmachine.rb
@@ -5,6 +5,7 @@ elsif RUBY_PLATFORM =~ /java/
   require 'jeventmachine'
 else
   begin
+    $:.unshift File.join(File.dirname(__FILE__),'..','ext')
     require 'rubyeventmachine'
   rescue LoadError
     warn "Unable to load the EventMachine C extension; To use the pure-ruby reactor, require 'em/pure_ruby'"
